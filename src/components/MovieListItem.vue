ec8ba8e27ccdf7f83c9e874e053bd3af9efcfc17
Jaegu Kang <kjaegu9771@naver.com>
2020-06-04 09:25:02 +0900

    re deploy

--- src/components/MovieListItem.vue
+++ src/components/MovieListItem.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="col-3">
-    <div class="card p-3" style="width: 18rem;">
-      <img :src="movie.poster_url" class="card-img-top" alt="Movie Poster">
-      <div class="card-body">
-        <h5 class="card-title">{{ movie.title }}</h5>
-        <MovieListItemModal :movie="movie"/>
-      </div>
+  <div class="card col-sm-12 col-lg-3" style="width: 18rem;">
+    <img :src="movie.poster_url" class="card-img-top" alt="Movie Poster">
+    <div class="card-body">
+      <h5 class="card-title">{{ movie.title }}</h5>
+      <MovieListItemModal :movie="movie"/>
     </div>
   </div>
 </template>
