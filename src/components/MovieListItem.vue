4509e78713753faacc377b2280c675bbd2d438a1
Jaegu Kang <kjaegu9771@naver.com>
2020-06-04 09:31:48 +0900

    Grid System

--- src/components/MovieListItem.vue
+++ src/components/MovieListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card col-sm-12 col-lg-3" style="width: 18rem;">
+  <div class="card col-12 col-sm-6 col-lg-3" style="width: 18rem;">
     <img :src="movie.poster_url" class="card-img-top" alt="Movie Poster">
     <div class="card-body">
       <h5 class="card-title">{{ movie.title }}</h5>
